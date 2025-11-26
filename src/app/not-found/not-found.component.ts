ba2f47df96ff405e1db51d88010c4f44fb38be3b
Mohammad Salameen <anas-20111-2011@hotmail.com>
2025-11-26 23:06:36 +0200

    Add animation style for the navbar and not-found page

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { fadeIn, slideInUp } from '../shared/animations';
 
 @Component({
   selector: 'app-not-found',
   imports: [RouterLink],
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
+  styleUrl: './not-found.component.css',
+  animations:[fadeIn, slideInUp]
 })
 export class NotFoundComponent {
 
