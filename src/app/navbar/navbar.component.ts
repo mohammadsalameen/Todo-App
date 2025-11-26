f016f1c4c2ca62c670a9ddc933d487eee3702b9b
Mohammad Salameen <anas-20111-2011@hotmail.com>
2025-11-26 23:32:47 +0200

    Add animation to navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,12 +3,14 @@ import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 import { AuthService } from '../services/auth-service.service';
 import { navLinks } from '../constants';
 import { NgFor } from '@angular/common';
+import { fadeIn } from '../shared/animations';
 
 @Component({
   selector: 'app-navbar',
   imports: [RouterLink, RouterLinkActive, NgFor],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
+  animations:[fadeIn]
 })
 export class NavbarComponent {
   constructor(private authService: AuthService, private router: Router){}
