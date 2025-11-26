334db34f0547122c93052ecefe071c4b9109eedc
Mohammad Salameen <anas-20111-2011@hotmail.com>
2025-11-26 16:25:22 +0200

    Add a constant file

---
+++ src/app/constants.ts
@@ -0,0 +1,7 @@
+export const navLinks = [
+  { label: 'Home', route: '/', exact: true },
+  { label: 'Dashboard', route: '/admin/dashboard', exact: false },
+  { label: 'Users', route: '/admin/users-list', exact: false },
+  { label: 'Tasks Summary', route: '/tasks-summary', exact: false },
+  { label: 'Add Task', route: '/admin/add-task', exact: false },
+];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 import { AuthService } from '../services/auth-service.service';
+import { navLinks } from '../constants';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgFor],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   constructor(private authService: AuthService, private router: Router){}
-
+  links = navLinks;
   logout(){
     this.authService.logout();
     this.router.navigate(['/auth'])
