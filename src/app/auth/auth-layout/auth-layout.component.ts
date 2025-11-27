5a25b3f132f5069b3d56ae3d4dcf35ae8112b92e
Mohammad Salameen <anas-20111-2011@hotmail.com>
2025-11-27 10:04:21 +0200

    Create a reusable form component for sign in and sign up

--- src/app/auth/auth-layout/auth-layout.component.ts
+++ src/app/auth/auth-layout/auth-layout.component.ts
@@ -1,20 +1,30 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from '../login/login.component';
-import { SignupComponent } from '../sign-up/sign-up.component';
 import { fadeIn } from '../../shared/animations';
+import { AuthFormComponent } from "../../shared/auth-form/auth-form.component";
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-layout',
   templateUrl: './auth-layout.component.html',
   styleUrls: ['./auth-layout.component.css'],
   standalone: true,
-  imports: [LoginComponent, SignupComponent],
+  imports: [AuthFormComponent],
   animations: [fadeIn]
 })
 export class AuthLayoutComponent {
   isSignUpActive = false;
+  signUpTitle: string = 'Sign Up';
+  signInTitle: string = 'Sign In'
 
   toggle(signUp: boolean) {
     this.isSignUpActive = signUp;
+  }
+
+  signUpForm(form: NgForm){
+    console.log(form);
+  }
+
+  signInForm(form: NgForm){
+    console.log(form);
   }
 }
--- src/app/auth/auth.route.ts
+++ src/app/auth/auth.route.ts
@@ -1,19 +1,9 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './sign-up/sign-up.component';
 import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 
 export const authRoutes: Routes = [
   {
     path: '',
     component: AuthLayoutComponent,
-  },
-  {
-    path: 'signin',
-    component: LoginComponent,
-  },
-  {
-    path: 'signup',
-    component: SignupComponent
   },
 ];
